8b1d741231c05b88e5a6f1525097d29608913545
Nadeera Nethmina <nadeeranethmina2004@gmail.com>
2026-05-12 01:21:53 +0530

    ui: add topbar and breadcrumbs for damaged items management🫡🤍

---
+++ frontend/modules/manager/damaged_item.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Damage Item Management</title>
+
+    <style>
+
+
+    </style>
+</head>
+<body>
+    <div class="main">
+  <header class="topbar">
+    <div class="breadcrumb">
+      <span>Inventory</span>
+      <span class="breadcrumb-sep">›</span>
+      <span class="active">Damaged Items Management</span>
+    </div>
+</header>
+</body>
+</html>
