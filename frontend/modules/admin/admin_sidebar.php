6c33c84b7161bd89b789dcdc53f61b9221f467d1
pasindumethmadu2004-rgb <pasindumethmadu2004@gmail.com>
2026-05-13 23:13:09 +0530

    create side bar

---
+++ frontend/modules/admin/admin_sidebar.php
@@ -0,0 +1,26 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SideBar</title>
+ </head>
+ <body>
+
+ <div class="sidebar">
+        <h2>  Admin Panel</h2>
+        <ul>
+            <li> <a href="#users">  User Management</a> </li>
+            <li> <a href="#branches">  Branch Management</a> </li>
+            <li> <a href="#products">  Product Management</a> </li>
+            <li> <a href="#inventory">  Inventory Overview</a> </li>
+            <li> <a href="#suppliers">  Supplier Management</a> </li>
+            <li> <a href="#reports">  Reports & Analytics</a> </li>
+        </ul>
+    </div>
+    
+ </body>
+ </html>
+ 
+
+ 
