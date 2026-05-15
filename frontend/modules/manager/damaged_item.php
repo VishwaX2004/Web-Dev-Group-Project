86609c9de22ef519b33f3b0e638528fe5f7b511c
Nadeera Nethmina <nadeeranethmina2004@gmail.com>
2026-05-15 15:47:49 +0530

    updates for edit icon and delete icon 😋🤍

--- frontend/modules/manager/damaged_item.php
+++ frontend/modules/manager/damaged_item.php
@@ -158,7 +158,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <button class='icon-btn' onclick='editItem(\"".$row['damage_id']."\", \"".$row['branch_id']."\", \"".$row['product_id']."\", \"".$row['quantity']."\", \"".$safe_reason."\", \"".$row['reported_date']."\")'>
                       <svg viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\"><path d=\"M11 4H4a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7\"/><path d=\"M18.5 2.5a2.121 2.121 0 013 3L12 15l-4 1 1-4 9.5-9.5z\"/></svg>
                     </button>
-                    <button class='icon-btn' style='color:var(--danger);' onclick='deleteItem(\"".$row['damage_id']."\")'>
+                    <button class='icon-btn delete-btn' onclick='deleteItem(\"".$row['damage_id']."\")'>
                       <svg viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\"><polyline points=\"3 6 5 6 21 6\"/><path d=\"M19 6l-1 14a2 2 0 01-2 2H8a2 2 0 01-2-2L5 6\"/><path d=\"M10 11v6\"/><path d=\"M14 11v6\"/><path d=\"M9 6V4a1 1 0 011-1h4a1 1 0 011 1v2\"/></svg>
                     </button>
                   </div>
