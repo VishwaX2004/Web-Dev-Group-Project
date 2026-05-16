237c37d44d519a87e2ce49bb3d1f7930ff6e8cb7
pasindumethmadu2004-rgb <pasindumethmadu2004@gmail.com>
2026-05-16 11:41:39 +0530

    side bar change

--- frontend/includes/admin_sidebar.php
+++ frontend/includes/admin_sidebar.php
@@ -9,7 +9,10 @@
 
  <div class="sidebar">
     <div class="top-section">
-        <h2>  Admin Panel</h2>
+        <h2><a href="/Web-Dev-Group-Project/frontend/modules/admin/admin_dashboard.php">
+            Admin Panel
+        </a>
+    </h2>
         <ul>
             <li><a href="/Web-Dev-Group-Project/frontend/modules/admin/user_management_frontend.php">User Management</a></li>
             <li><a href="/Web-Dev-Group-Project/frontend/modules/admin/branch_management_frontend.php">Branch Management</a></li>
