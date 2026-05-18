ae0163e7b3f95a09aa75eb9aa799a2b10b144ff2
Vishwa_Pramuditha <vishwapramuditha200@gmail.com>
2026-05-18 17:33:05 +0530

    Admin Dashboard Fix ✅💕

--- frontend/includes/admin_sidebar.php
+++ frontend/includes/admin_sidebar.php
@@ -9,21 +9,21 @@
 
  <div class="sidebar">
     <div class="top-section">
-        <h2><a href="../../modules/admin/admin_dashboard.php">
+        <h2>
             Admin Panel
-        </a>
     </h2>
         <ul>
-            <li><a href="../../modules/admin/user_management_frontend.php">User Management</a></li>
-            <li><a href="../../modules/admin/branch_management_frontend.php">Branch Management</a></li>
-            <li> <a href="../../modules/admin/product_management.php">Product Management</a> </li>
-            <li> <a href="../../modules/admin/admin_inventory_overview.php">Inventory Overview</a> </li>
-            <li> <a href="../../modules/admin/admin_supplier_managment.php">Supplier Management</a> </li>
-            <li> <a href="../../modules/admin/Reports_and_Analytics.php">Reports & Analytics</a> </li>
+            <li><a href="/Web-Dev-Group-Project/frontend/modules/admin/admin_dashboard.php">Admin dashboard</a></li>
+            <li><a href="/Web-Dev-Group-Project/frontend/modules/admin/user_management_frontend.php">User Management</a></li>
+            <li><a href="/Web-Dev-Group-Project/frontend/modules/admin/branch_management_frontend.php">Branch Management</a></li>
+            <li> <a href="/Web-Dev-Group-Project/frontend/modules/admin/product_management.php">  Product Management</a> </li>
+            <li> <a href="/Web-Dev-Group-Project/frontend/modules/admin/admin_inventory_overview.php">  Inventory Overview</a> </li>
+            <li> <a href="/Web-Dev-Group-Project/frontend/modules/admin/admin_supplier_managment.php">  Supplier Management</a> </li>
+            <li> <a href="/Web-Dev-Group-Project/frontend/modules/admin/Reports_and_Analytics.php">  Reports & Analytics</a> </li>
         </ul>
     </div>
         <div class="logout-section">
-  <a href="../../../backend/api/logout.php" class="logout-btn">LOGOUT</a>
+  <a href="/Web-Dev-Group-Project/backend/api/logout.php" class="logout-btn">LOGOUT</a>
         </div>
 </div>
     
