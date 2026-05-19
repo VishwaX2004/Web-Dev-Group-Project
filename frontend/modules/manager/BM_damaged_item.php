d7d800f95aa00df2ad842041f4b952373b1fee29
Nadeera Nethmina <nadeeranethmina2004@gmail.com>
2026-05-19 06:56:58 +0530

    Update few lines🤐🤍

--- frontend/modules/manager/BM_damaged_item.php
+++ frontend/modules/manager/BM_damaged_item.php
@@ -189,11 +189,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           </div>
           <div class="form-group">
             <label class="form-label">Branch ID *</label>
-            <input class="form-input" name="branch_id" id="fbrancId" type="text" placeholder="e.g. BR-001" required>
+            <input class="form-input" name="branch_id" id="fbrancId" type="text" placeholder="e.g. B001" required>
           </div>
           <div class="form-group">
             <label class="form-label">Product ID *</label>
-            <input class="form-input" name="product_id" id="fProductId" type="text" placeholder="e.g. PR0D-1234" required>
+            <input class="form-input" name="product_id" id="fProductId" type="text" placeholder="e.g. P123" required>
           </div>
           <div class="form-group">
             <label class="form-label">Quantity *</label>
